1e6282d4d574387ef311584e62aa1e8c4f6d9521
Zinksta <mzink79@gmail.com>
2016-11-18 11:31:38 +1000

    Add project files.

---
+++ ADS_Linked List/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace ADS_Linked_List
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExecute = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnExecute
+            // 
+            this.btnExecute.Location = new System.Drawing.Point(12, 39);
+            this.btnExecute.Name = "btnExecute";
+            this.btnExecute.Size = new System.Drawing.Size(75, 23);
+            this.btnExecute.TabIndex = 0;
+            this.btnExecute.Text = "&Execute";
+            this.btnExecute.UseVisualStyleBackColor = true;
+            this.btnExecute.Click += new System.EventHandler(this.btnExecute_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExit.Location = new System.Drawing.Point(497, 326);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtOutput.Location = new System.Drawing.Point(93, 39);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(479, 281);
+            this.txtOutput.TabIndex = 2;
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(90, 9);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(39, 13);
+            this.lblOutput.TabIndex = 3;
+            this.lblOutput.Text = "Output";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnExecute);
+            this.MinimumSize = new System.Drawing.Size(600, 400);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Linked List";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExecute;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Label lblOutput;
+    }
+}
+
---
+++ ADS_Linked List/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ADS_Linked_List
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        // First create a node class. This class would instantiate a node 
+        // when we want to add a new node to our linked list.
+        public class Node
+        {
+            public object data; //to store the data of the node
+            public Node link; //the reference(link) of the node
+            public Node(object data) //constructor
+            {
+                this.data = data;
+            }
+        }
+
+        /*The second class that we create is the LinkedList class. 
+        The main purpose of this class is to set the references/links of the nodes.
+        We also include all methods that manipulate the nodes and their links in this class.*/
+        public class LinkedList
+        {
+            Node head; //the first node of the linked list
+            Node current; //the last added node
+            // The Add method
+            public void Add(Node node1) //add a new node
+            {
+                if (head == null)
+                {
+                    head = node1; //head is pointed to the newly added node
+                    current = head; //current is pointed to the newly added node
+                }
+                else
+                {
+                    current.link = node1; //a link is made to the newly added node
+                    current = current.link; //current is updated
+                }
+            }
+            // The Print method
+            public String Print() //print all nodes in the linked list
+            {
+                Node PrintNode = head;
+                String output = "";
+                while (PrintNode != null)
+                {
+                    output += PrintNode.data + "\r\n";
+                    PrintNode = PrintNode.link;
+                }
+                return output;
+            }
+        }
+            private void btnExecute_Click(object sender, EventArgs e)
+        {
+            LinkedList list = new LinkedList();
+
+            //add new nodes
+            list.Add(new Node("Apple"));
+            list.Add(new Node("Orange"));
+            list.Add(new Node("Pears"));
+            list.Add(new Node("Grapes"));
+            list.Add(new Node("Strawberry"));
+
+            //print nodes
+            txtOutput.Text = list.Print();
+        }
+    }
+}
